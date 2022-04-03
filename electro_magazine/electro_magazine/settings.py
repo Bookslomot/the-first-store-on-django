653b9d468bff3902e4051a36be912691e877062f
bookslomot <alexgol2101@gmail.com>
2022-04-03 15:05:55 +0400

    test update in settings

--- electro_magazine/electro_magazine/settings.py
+++ electro_magazine/electro_magazine/settings.py
@@ -31,15 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cart.apps.CartConfig',
-    'account.apps.AccountConfig',
-    'shop.apps.ShopConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'cart.apps.CartConfig',
+    'account.apps.AccountConfig',
+    'shop.apps.ShopConfig',
 
 ]
 
